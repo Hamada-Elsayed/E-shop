3d631d0b0751d9d3c61c706b0074354b409d1327
Hamada Elsayed <me0099278@gmail.com>
2024-08-17 20:38:04 +0300

    update backend url

--- src/API/baseURL.js
+++ src/API/baseURL.js
@@ -1,4 +1,4 @@
 import axios from "axios";
-const baseURL = axios.create({ baseURL: 'https://backend-46p3.vercel.app' })
+const baseURL = axios.create({ baseURL: 'https://backend-7szr8id0p-hamadas-projects-1ea8d8e6.vercel.app' })
 
 export default baseURL;
